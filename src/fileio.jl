15889d02c8de585ca4e589e5933a7f583e13ac01
Austin Zadoks <austin.zadoks@protonmail.com>
2023-07-06 13:08:19 +0200

    Add comments, small refactoring of save*

--- src/fileio.jl
+++ src/fileio.jl
@@ -8,6 +8,13 @@ const LENGTH_UNIT = u"Å"
 const FORCE_UNIT = u"Eh_au" / u"Å"
 const KNOWN_PERIODIC_KEYWORDS = ("PRIMVEC", "CONVVEC", "PRIMCOORD", "CONVCOORD")
 
+# Cound the number of boundary conditions which are AtomsBase.Periodic
+function count_periodic_bcs(system::AbstractSystem)
+    return count(Base.Fix2(isa, Periodic), boundary_conditions(system))
+end
+
+# Custom version of `iterate(::Base.EachLine)` which cleans lines for parsing
+# and skips comment / empty lines
 function iterate_xsf(itr::Base.EachLine)
     eof(itr.stream) && return itr.ondone()
     line = readline(itr.stream; keep=itr.keep)
@@ -16,6 +23,10 @@ function iterate_xsf(itr::Base.EachLine)
     return string(clean_line)
 end
 
+# Get boundary conditions in the XSF convention from the system type keyword
+# NOTE: the XSF documentation talks about a keyword `MOLECULE`, but it isn't
+# mentioned in the ase.io parser nor in any of the examples, so it is not
+# implemented here
 function parse_boundary_conditions(line)
     occursin("ATOMS", line) && return [DirichletZero(), DirichletZero(), DirichletZero()]
     occursin("POLYMER", line) && return [Periodic(), DirichletZero(), DirichletZero()]
@@ -24,12 +35,17 @@ function parse_boundary_conditions(line)
     return error("Unknown structure type $(line)")
 end
 
+# Parse blocks like PRIMVEC and CONVVEC
 function parse_vec_block(T::Type{<:Real}, lines)
     return map(1:3) do _
         return parse.(T, split(iterate_xsf(lines))) .* LENGTH_UNIT
     end
 end
 
+# Parse an atomic position line, which is formed as either:
+# atomic_number x y z
+# or
+# atomic_number x y z Fx Fy Fz
 function parse_coord_line(T::Type{<:Real}, line)
     words = split(line)
     number = parse(Int, words[1])
@@ -43,6 +59,8 @@ function parse_coord_line(T::Type{<:Real}, line)
     end
 end
 
+# Parse an entire block of atomic coordinates in a periodic system,
+# i.e. when the number of atoms is provided explicitly
 function parse_coord_block(T::Type{<:Real}, lines)
     line = iterate_xsf(lines)
     n_atoms = parse(Int, first(split(line)))
@@ -51,6 +69,9 @@ function parse_coord_block(T::Type{<:Real}, lines)
     end
 end
 
+# Parse an arbitrary block from a periodic file
+# Supported keywords are PRIMVEC, CONVVEC, PRIMCOORD, and CONVCOORD, i.e.
+# DATAGRID_[2,3]D blocks are not (yet) supported
 function parse_xsf_block(T::Type{<:Real}, keyword, lines)
     if keyword in ("PRIMVEC", "CONVVEC")
         return parse_vec_block(T, lines)
@@ -61,24 +82,31 @@ function parse_xsf_block(T::Type{<:Real}, keyword, lines)
     end
 end
 
+# Parse a frame (ANIMSTEP in XSF lingo) from a periodic file
+# The first frame will have at least PRIMVEC and PRIMCOORD and possibly CONVVEC
+# and CONVCOORD blocks
+# Following frames will have at least PRIMCOORD (PRIMVEC and others are optional)
+# If a frame doesn't have PRIMVEC, the bounding box (PRIMVEC) from the previous frame needs
+# to be brought forward
 function parse_periodic_frame(T::Type{<:Real}, lines, bcs, previous_frame)
     should_parse = true
     i = 0
     io_pos = position(lines.stream)
     blocks = Dict()
-    while should_parse && i <= 4
+    while should_parse && i <= 4  # Maximum 4 blocks (PRIMVEC, PRIMCOORD, CONVVEC, CONVCOORD)
         line = iterate_xsf(lines)
-        if isnothing(line)
+        if isnothing(line)  # We've reached the end of the file
             should_parse = false
         elseif (keyword = first(split(line))) in KNOWN_PERIODIC_KEYWORDS
             if haskey(blocks, keyword)
+                # If we've already seen this keyword, we're in the next block
                 should_parse = false
-                seek(lines.stream, io_pos)
+                seek(lines.stream, io_pos)  # Go back to the keyword line
             else
                 blocks[keyword] = parse_xsf_block(T, keyword, lines)
                 io_pos = position(lines.stream)
             end
-        else
+        else  # We've reached the end of the frame
             should_parse = false
             seek(lines.stream, io_pos)
         end
@@ -94,9 +122,11 @@ function parse_periodic_frame(T::Type{<:Real}, lines, bcs, previous_frame)
     return atomic_system(blocks["PRIMCOORD"], blocks["PRIMVEC"], bcs)
 end
 
+# Parse a frame (ANIMSTEP in XSF lingo) from a non-periodic file (ATOMS)
 function parse_atoms_frame(T::Type{<:Real}, lines)
     line = iterate_xsf(lines)
     atoms = []
+    # Go until the end of the file or until we see another frame (ATOMS) or data grid (BEGIN)
     while !isnothing(line) && !startswith(line, "ATOMS") && !startswith(line, "BEGIN")
         atom = parse_coord_line(T, line)
         push!(atoms, atom)
@@ -105,9 +135,12 @@ function parse_atoms_frame(T::Type{<:Real}, lines)
     return isolated_system(atoms)
 end
 
-# Single structure
+# Load all the frames from an XSF file
 function load_xsf(T::Type{<:Real}, file::Union{AbstractString,IOStream})
     lines = eachline(file; keep=false)
+    # The first line should be "ANIMSTEPS [N]" for a trajectory (in which case, the system
+    # type is in the second line) or one of the system type keywords (ATOMS, POLYMER, SLAB,
+    # CRYSTAL), in which case the number of frames is 1
     line = iterate_xsf(lines)
     if occursin("ANIMSTEPS", line)
         n_frames = parse(Int, last(split(line)))
@@ -118,15 +151,21 @@ function load_xsf(T::Type{<:Real}, file::Union{AbstractString,IOStream})
     bcs = parse_boundary_conditions(line)
     frames = AbstractSystem{3}[]
     for _ in 1:n_frames
-        if count(Base.Fix2(isa, AtomsBase.Periodic), bcs) > 0 # CRYSTAL, SLAB, or POLYMER
+        # Check how many boundary conditions are Periodic to determine whether we have
+        # ATOMS or one of POLYMER, SLAB, CRYSTAL
+        if count(Base.Fix2(isa, AtomsBase.Periodic), bcs) == 0
+            push!(frames, parse_atoms_frame(T, lines))
+        else
+            # We need to pass the previous frame because if the unit cell is fixed,
+            # it is written only in the first frame and we need to pass it through
+            # frame-by-frame
             previous_frame = isempty(frames) ? nothing : last(frames)
             push!(frames, parse_periodic_frame(T, lines, bcs, previous_frame))
-        else  # ATOMS
-            push!(frames, parse_atoms_frame(T, lines))
         end
     end
-    return length(frames) == 1 ? first(frames) : frames
+    return frames
 end
+# Set a default floating-point type of Float64
 load_xsf(file::Union{AbstractString,IOStream}) = load_xsf(Float64, file)
 
 function write_system_type(io::IO, n_periodic_bcs::Int)
@@ -144,43 +183,54 @@ function write_atom(io::IO, atom)
         @printf io "%3d %20.14f %20.14f %20.14f\n" n x y z
     end
 end
+
 function write_atoms(io::IO, system::AbstractSystem; header="")
-    !isempty(header) && println(io, header)
+    !isempty(header) && println(io, strip(header))
     return map(Base.Fix1(write_atom, io), system[:])
 end
 
 function write_bounding_box(io::IO, system::AbstractSystem; header="")
-    !isempty(header) && println(io, header)
+    !isempty(header) && println(io, strip(header))
     for i in 1:3
         x, y, z = ustrip(uconvert.(LENGTH_UNIT, bounding_box(system)[i]))
         @printf io "%20.14f %20.14f %20.14f\n" x y z
     end
 end
 
-function save_xsf(io::IO, system::AbstractSystem{3}; frame="")
-    bcs = boundary_conditions(system)
-    n_periodic_bcs = count(Base.Fix2(isa, Periodic), bcs)
-    if n_periodic_bcs == 0
+function write_periodic_frame(io::IO, system::AbstractSystem; frame="")
+    n_atoms = length(system)
+    write_bounding_box(io, system; header="PRIMVEC $(frame)")
+    write_bounding_box(io, system; header="CONVVEC $(frame)")
+    write_atoms(io, system; header="PRIMCOORD $(frame)\n$(n_atoms) 1")
+end
+
+function write_frame(io::IO, system::AbstractSystem; frame="")
+    if count_periodic_bcs(system) == 0
         write_atoms(io, system; header="ATOMS $(frame)")
     else
-        n_atoms = length(system)
-        if isempty(frame) || isnothing(frame) || frame == 1
-            write_system_type(io, n_periodic_bcs)
+        write_periodic_frame(io, system; frame)
+    end
+end
+
+function save_xsf(io::IO, system::AbstractSystem)
+    count_periodic_bcs(system) > 0 && write_system_type(io, n_periodic_bcs)
+    write_frame(io, system)
+end
+
+function save_xsf(io::IO, frames::AbstractVector{<:AbstractSystem})
+    if length(frames) == 1
+        # If we get a single frame, just write it as a bare structure (not as an animation)
+        return save_xsf(io, only(frames))
+    else
+        # Otherwise, we write the animation line followed by each frame in sequence
+        println(io, "ANIMSTEPS $(length(frames))")
+        for i in eachindex(frames)
+            write_frame(io, frames[i]; frame=i)
         end
-        write_bounding_box(io, system; header="PRIMVEC $(frame)")
-        write_bounding_box(io, system; header="CONVVEC $(frame)")
-        write_atoms(io, system; header="PRIMCOORD $(frame)\n$(n_atoms) 1")
     end
 end
 
-function save_xsf(io::IO, frames)
-    println(io, "ANIMSTEPS $(length(frames))")
-    for i in eachindex(frames)
-        save_xsf(io, frames[i]; frame=i)
-    end
-end
-
-function save_xsf(file::AbstractString, frames)
+function save_xsf(file::AbstractString, frames::AbstractVector{<:AbstractSystem})
     open(file, "w") do io
         save_xsf(io, frames)
     end
