8da1e8dda9f7099017ac117536edaaceb1d0d2c4
Austin Zadoks <austin.zadoks@protonmail.com>
2023-08-14 13:45:12 +0200

    Cleaner saving code; warnings for unsupported properties; return empty structure list on parsing failure

--- src/XCrySDenStructureFormat.jl
+++ src/XCrySDenStructureFormat.jl
@@ -1,5 +1,14 @@
 module XCrySDenStructureFormat
 
+using AtomsBase
+using Unitful
+using UnitfulAtomic
+using PeriodicTable: PeriodicTable
+using StaticArrays
+
+const LENGTH_UNIT = u"Å"
+const FORCE_UNIT = u"Eh_au" / u"Å"
+
 export load_xsf
 export save_xsf
 include("fileio.jl")
